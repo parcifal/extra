ea2aae7acd7d1a1584966ac0ffc0cb11367becde
Michaël van de Weerd <com@michaelvandeweerd.net>
2016-01-18 15:38:06 +0100

    Removed the default wait period after every act

--- src/eu/parcifal/extra/thread/Runner.java
+++ src/eu/parcifal/extra/thread/Runner.java
@@ -12,31 +12,14 @@ import eu.parcifal.extra.throwing.MethodNotImplementedException;
  */
 public abstract class Runner implements Runnable {
 	/**
-	 * The default length of the act period.
-	 */
-	private static final long DEFAULT_ACT_PERIOD = 50;
-
-	/**
 	 * The default length of the pause period.
 	 */
 	private static final long DEFAULT_PAUSE_PERIOD = 25;
-
-	/**
-	 * The warning message for when the execution time of the act method is
-	 * greater than the act period.
-	 */
-	private static final String WARNING_OVERTIME = "the execution time of method act() took %1$sms longer than the current act period of %2$sms, consider raising its value using setActPeriod(long)";
-
 	/**
 	 * The warning message for when the runner has been interrupted while
 	 * running.
 	 */
 	private static final String WARNING_INTERRUPTED = "the runner has been interrupted by %1$s and will stop running";
-
-	/**
-	 * The act period of the current runner.
-	 */
-	private long actPeriod = DEFAULT_ACT_PERIOD;
 
 	/**
 	 * The pause period of the current runner.
@@ -62,16 +45,6 @@ public abstract class Runner implements Runnable {
 	 * The amount of times the act method has been executed.
 	 */
 	private int actCycle = 0;
-
-	/**
-	 * Set the act period of the current runner to the specified length.
-	 * 
-	 * @param length
-	 *            The new length of the act period of the current runner.
-	 */
-	public void setActPeriod(long length) {
-		this.actPeriod = length;
-	}
 
 	/**
 	 * Set the pause period of the current runner to the specified length.
@@ -154,39 +127,22 @@ public abstract class Runner implements Runnable {
 				Console.debug(mnie.getMessage());
 			}
 
-			// used during this run for the calculation of the remaining time of
-			// the act period
-			long start, sleep;
-
 			while (this.running) {
-				start = System.currentTimeMillis();
-
 				this.runCycle++;
 
 				if (!this.paused) {
 					this.act();
-					this.actCycle++;
-
-					sleep = actPeriod - (System.currentTimeMillis() - start);
 				} else {
-					sleep = pausePeriod - (System.currentTimeMillis() - start);
-				}
-
-				if (sleep >= 0) {
-					Thread.sleep(sleep);
-				} else {
-					Console.warn(Level.HIGH, WARNING_OVERTIME, -sleep, this.actPeriod);
+					this.wait(pausePeriod);
 				}
 			}
-		} catch (InterruptedException exception) {
-			Console.warn(Level.FATAL, WARNING_INTERRUPTED, exception.getCause());
-
-			this.stop();
+		} catch (InterruptedException e) {
+			Console.warn(Level.FATAL, WARNING_INTERRUPTED);
 		} finally {
 			try {
 				this.finalise();
 			} catch (MethodNotImplementedException mnie) {
-				Console.debug(mnie);
+				Console.debug(mnie.getMessage());
 			}
 		}
 	}
